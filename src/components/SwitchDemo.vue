05b9d04a4910d94c745554735103f28a015a420e
Ories Zhu <tellt@126.com>
2020-11-22 20:10:44 +0800

    给switch组件添加属性说明

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -3,23 +3,37 @@
     <h1>Switch 组件示例 </h1>
     <Demo :component="Switch1Demo" />
     <Demo :component="Switch2Demo" />
+    <attr :columns="columns" :data="data"></attr>
+
   </div>
 </template>
 
 <script lang="ts">
   import Switch1Demo from './Switch1.demo.vue'
   import Switch2Demo from './Switch2.demo.vue'
-  import Demo from './Demo.vue'
+  import Attr from './Attr.vue'
 
-  const Prism = (window as any).Prism;
-  console.log(window.Prism);
+  import Demo from './Demo.vue'
+  import {columns} from "../lib/data";
+
+  // const Prism = (window as any).Prism;
+  // console.log('fuck0',window.Prism);
   export default {
     components: {
-      Demo
+      Demo, Attr
     },
     setup() {
+      const data = [
+        {
+          params: 'value',
+          desc: '是否选中',
+          type: 'boolean',
+          select: 'false / true',
+          default: 'false',
+        }
+      ];
       return {
-        Switch1Demo,Switch2Demo
+        Switch1Demo,Switch2Demo,data,columns
       }
     }
   }
