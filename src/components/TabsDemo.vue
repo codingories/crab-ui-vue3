3cfacf18cb04b3eb5e53d5126956f1e1bf42831c
Ories Zhu <tellt@126.com>
2020-11-22 20:17:59 +0800

    给tab添加属性说明

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,18 +1,33 @@
 <template>
   <h1>Tabs 示例</h1>
   <Demo :component="Tabs1Demo" />
+  <attr :columns="columns" :data="data"></attr>
 </template>
 
 <script lang="ts">
   import Demo from './Demo.vue'
   import Tabs1Demo from './Tabs1.demo.vue'
+  import Attr from './Attr.vue'
+  import {columns} from "../lib/data";
+
   export default {
     components: {
-      Demo
+      Demo,Attr
     },
     setup() {
+      const data = [
+        {
+          params: 'Tab',
+          desc: 'tab页签',
+          type: 'component',
+          select: 'Tab',
+          default: '--',
+        },
+      ];
       return {
-        Tabs1Demo
+        Tabs1Demo,
+        columns,
+        data
       }
     }
   }
